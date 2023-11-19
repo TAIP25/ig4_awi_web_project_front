0d81f3f3731a8eb93d98b4ca44b696864b675b30
Robin <robin.ave28@gmail.com>
2023-11-19 18:19:30 +0100

    Update Login.tsx

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,11 +1,123 @@
-import "./Login.scss";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import {decodeToken} from 'react-jwt';
+import Cookies from 'js-cookie';
+import { useState, useEffect, useContext} from 'react';
 
-function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+
+
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function Login() {
+    
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    
+    if (data.get('email') == "" || data.get('password') == "") {
+        alert("Veuillez remplir tous les champs");
+    }
+
+    axios.post(`${import.meta.env.VITE_API_URL}/benevoles/login/`, {
+        email: data.get('email'),
+        password: data.get('password'),
+    }).then((response) => {
+        console.log(response);
+        const {token} = response.data;
+        const decodedToken : any = decodeToken(token);
+        const id_benevole = decodedToken.id_benevole;
+        Cookies.set('token', token, {expires: 1, secure: true});
+        Cookies.set('id_member', id_benevole, {expires: 1, secure: true});
+        console.log("Utilisateur connecté !");
+        console.log("Token : " + token);
+        console.log("Id : " + id_benevole);
+      
+    }).catch((error) => {
+        console.log(error);
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Connectez-vous
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Mot de passe"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Se souvenir de moi"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Connection
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Mot de passe oublié ?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Pas de compte ? Inscrivez-vous"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default Login;
