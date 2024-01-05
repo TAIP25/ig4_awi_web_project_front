d1206d0b3716ae59c059ad007a44fb278fbcd474
Robinkss <robin.ave28@gmail.com>
2024-01-05 23:29:08 +0100

    Ajout du context pour auth + style du component Account

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+type Props = {
+    children?: React.ReactNode;
+};
+
+
+type IAuthContext = {
+    isAuthenticated: boolean;
+    setIsAuthenticated: (value: boolean) => void;
+}
+
+const initialState: IAuthContext = {
+    isAuthenticated: false,
+    setIsAuthenticated: () => { }
+}
+
+
+
+const AuthContext = createContext<IAuthContext>(initialState);
+
+export const AuthProvider = ({ children }: any) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
