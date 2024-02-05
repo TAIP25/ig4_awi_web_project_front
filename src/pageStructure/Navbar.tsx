af66e8007f58994ad7f00bb58bbfa7c3c8b7766d
TAIP25 <bouleon25@gmail.com>
2024-02-05 07:36:40 +0100

    fix(Main Page)

--- src/pageStructure/Navbar.tsx
+++ src/pageStructure/Navbar.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
@@ -17,24 +15,16 @@ import Cookies from 'js-cookie';
 import {decodeToken} from 'react-jwt';
 import axios from 'axios';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Stack } from '@mui/material';
+import { Divider, Stack } from '@mui/material';
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
   const {isAuthenticated, setIsAuthenticated} = useContext(AuthContext);
   const [isAdmin, setIsAdmin] = useState(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
@@ -80,131 +70,44 @@ function ResponsiveAppBar() {
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <img src="/vectoriel/logo_FDJ_FINAL.jpg" alt="logo" width="80px" height="47px" />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.2rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-              sx={{
-                transition: 'all 0.3s ease',
-                '&:hover': {
-                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                },
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              <MenuItem key="accueil" onClick={handleCloseNavMenu} >
-                <Typography 
-                  textAlign="center"
-                  component="a"
-                >
-                  <NavLink to="/" style={{color: 'inherit'}}>
-                    Accueil
-                  </NavLink>
+          <Stack direction="row" spacing={2} sx={{ flexGrow: 1, backgroundColor: 'transparent' }}>
+            <img src="/vectoriel/logo_FDJ_FINAL.jpg" alt="logo" width="80px" height="47px" />
+            <NavLink to="/" style={{color: 'inherit'}}>
+              <Button 
+                color="inherit"
+                sx={{
+                  transition: 'all 0.3s ease',
+                  '&:hover': {
+                    transform: 'scale(1.1)',
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                    border: '1px solid white',
+                  },
+                }}
+              >
+                <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold'}} >
+                  Accueil
                 </Typography>
-                </MenuItem>
-              <MenuItem key="calendrier" onClick={handleCloseNavMenu} >
-                <Typography 
-                textAlign="center" 
-                component="a"
-                  >
-                    <NavLink to="/planning" style={{color: 'inherit'}}>
-                      Calendrier
-                    </NavLink>
-                  </Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <Stack direction="row" spacing={2}>
-              <NavLink to="/" style={{color: 'inherit'}}>
-                <Button 
-                  color="inherit"
-                  sx={{
-                    transition: 'all 0.3s ease',
-                    '&:hover': {
-                      transform: 'scale(1.1)',
-                    },
-                  }}
-                >
-                  <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold'}} >
-                    Accueil
-                  </Typography>
-                </Button>
-              </NavLink>
-              <NavLink to="/planning" style={{color: 'inherit'}}>
-                <Button 
-                  color="inherit"
-                  sx={{
-                    transition: 'all 0.3s ease',
-                    '&:hover': {
-                      transform: 'scale(1.1)',
-                    },
-                  }}
-                >
-                  <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold'}} >
-                    Planning
-                  </Typography>
-                </Button>
-              </NavLink>
-            </Stack>
-          </Box>
-
+              </Button>
+            </NavLink>
+            <NavLink to="/planning" style={{color: 'inherit'}}>
+              <Button 
+                color="inherit"
+                sx={{
+                  transition: 'all 0.3s ease',
+                  '&:hover': {
+                    transform: 'scale(1.1)',
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                    border: '1px solid white',
+                  },
+                }}
+              >
+                <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold'}} >
+                  Planning
+                </Typography>
+              </Button>
+            </NavLink>
+          </Stack>
+          <Divider variant="middle" orientation="vertical" flexItem sx={{mx: 2, border: '1.5px solid white'}} />
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <Button onClick={handleOpenUserMenu} sx={{ color: 'white' }}>
@@ -239,7 +142,7 @@ function ResponsiveAppBar() {
                 [
                   <MenuItem key="account" onClick={handleCloseUserMenu}>
                     <NavLink to="/account" style={{color: 'inherit', textDecoration: 'none'}}>
-                      <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
+                      <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
                         MON COMPTE
                       </Typography>
                     </NavLink>
@@ -247,14 +150,14 @@ function ResponsiveAppBar() {
                   isAdmin && (
                     <MenuItem key="dashboard" onClick={handleCloseUserMenu} sx={{ display: 'flex', justifyContent: 'center' }}>
                       <NavLink to="/dashboard" style={{color: 'inherit', textDecoration: 'none'}}>
-                        <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
+                        <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
                           DASHBOARD
                         </Typography>
                       </NavLink>
                     </MenuItem>),
                   <MenuItem key="logout" onClick={handleLogout} sx={{ display: 'flex', justifyContent: 'center' }}>
                     <NavLink to="/login" style={{color: 'inherit', textDecoration: 'none'}}>
-                      <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
+                      <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
                         DECONNEXION
                       </Typography>
                     </NavLink>
@@ -264,14 +167,14 @@ function ResponsiveAppBar() {
                 [
                   <MenuItem key="login" onClick={handleCloseUserMenu} sx={{ display: 'flex', justifyContent: 'center' }}>
                     <NavLink to="/login" style={{color: 'inherit', textDecoration: 'none'}}>
-                      <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >             
+                      <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >             
                         CONNNECTION                  
                       </Typography>
                     </NavLink>
                   </MenuItem>,
                   <MenuItem key="register" onClick={handleCloseUserMenu} sx={{ display: 'flex', justifyContent: 'center' }}>
                     <NavLink to="/signup" style={{color: 'inherit', textDecoration: 'none', textAlign: 'center'}}>
-                      <Typography variant='h6' component="a" sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
+                      <Typography variant='h6' sx={{color: 'inherit', fontWeight: 'bold', textDecoration: 'none'}} >
                         INSCRIPTION
                       </Typography>
                     </NavLink>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,14 @@
 import { Box, Paper, Stack, Typography } from "@mui/material"
 
 import Switch from "../planning/Switch"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import MonInscriptionBenevole from "../../interfaces/MonInscriptionBenevole"
+import Festival from "../../interfaces/Festival"
+import axios from "axios"
+import { decodeToken } from "react-jwt"
+import Cookies from 'js-cookie';
+import CreneauHoraire from "../../interfaces/CreneauHoraire"
+import Poste from "../../interfaces/Poste"
 //import "./Home.scss"
 
 const NEWS = [
@@ -43,19 +50,6 @@ const REFERENTS = [
   }
 ]
 
-const PLANNING = [
-  {
-    id: 1,
-    title: "8h - 9h",
-    content: "Content 1"
-  },
-  {
-    id: 2,
-    title: "9h - 10h",
-    content: "Content 2"
-  }
-]
-
 
 export default function Home(){
   /* Variables */
@@ -63,7 +57,77 @@ export default function Home(){
   //Refactor with planning component
   const week: string[] = ["Samedi", "Dimanche"];
 
+  /* UseState */
   const [jour, setJour] = React.useState(week[0]);
+  const [myInscriptionBenevole, setMyInscriptionBenevole] = React.useState<MonInscriptionBenevole[]>([]);
+  const [creneauxHoraires, setCreneauxHoraires] = useState<CreneauHoraire[]>([]);
+  const [postes, setPostes] = useState<Poste[]>([]);
+  const [festival, setFestival] = React.useState<Festival | null>(null);
+
+  /* UseEffect */
+  // Fetch festival data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/festival/next`);
+        const festival: Festival = res.data;
+        setFestival(festival);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      if(festival === null) return;
+      try {
+        const benevoleID: number = getCookiesBenevoleID();
+        const [res1, res2, res3] = await Promise.all([
+          axios.get(`${import.meta.env.VITE_API_URL}/inscriptionbenevole/festival/${festival!.id}/benevole/${benevoleID}`),
+          axios.get(`${import.meta.env.VITE_API_URL}/poste/festival/${festival?.id}`),
+          axios.get(`${import.meta.env.VITE_API_URL}/creneauHoraire`)
+        ]);
+
+        const creneauxHoraires: CreneauHoraire[] = res3.data.creneauHoraire;
+        const postes: Poste[] = res2.data.postes;
+
+        let myInscriptionBenevoleTemp: MonInscriptionBenevole[] = res1.data.inscription.map((inscription: any) => {
+          const myInscriptionBenevole: MonInscriptionBenevole = {
+            id: inscription.id,
+            creneauHoraireID: inscription.creneauHoraireID,
+            posteID: inscription.posteID,
+            status: inscription.status,
+          };
+          return myInscriptionBenevole;
+        });
+
+        // Filter que les inscriptions Accepté
+        myInscriptionBenevoleTemp = myInscriptionBenevoleTemp.filter((inscription: MonInscriptionBenevole) => inscription.status === "Accepté");
+
+        setMyInscriptionBenevole(myInscriptionBenevoleTemp);
+        setCreneauxHoraires(creneauxHoraires);
+        setPostes(postes);
+        
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [festival]);
+
+  /* Functions */
+  function getCookiesBenevoleID() {
+    const decodedToken: any = decodeToken(Cookies.get('token')!)
+    const benevoleID: number = decodedToken.id_benevole
+    return benevoleID
+  }
+
+  function getCreneauHoraireByDay(day: string): CreneauHoraire[] {
+    return creneauxHoraires.filter((creneauHoraire) => creneauHoraire.jour === day)
+  }
 
   return(
     <Box
@@ -94,20 +158,22 @@ export default function Home(){
       </Typography>
       <Stack spacing={10} direction="row" sx={{ p: 10, marginLeft: '3rem', marginRight: '3rem' }}>
         <Stack spacing={2} sx={{ width: "100%" }}>
+        <Switch week={week} setJour={setJour} jour={jour} sx={{ alignSelf: "flex-start" }} />
           <Paper sx={{ p: 2, borderColor: "secondary.main", borderWidth: "2px", borderStyle: "solid" }}>
             <Typography variant="h6" sx={{ fontWeight: "bold" }} display={"flex"} justifyContent={"center"}>
               Planning
             </Typography>
-            <Switch week={week} setJour={setJour} jour={jour} sx={{ alignSelf: "flex-start" }} />
-            <Stack spacing={2} direction="row">
-              {PLANNING.map((planning) => (
-                <Paper key={planning.id} sx={{ p: 2, borderColor: "primary.main", borderWidth: "2px", borderStyle: "solid" }}>
+            <Stack spacing={2} direction="row" sx={{ justifyContent: "center" }}>
+              {getCreneauHoraireByDay(jour).map((creneauHoraire) => (
+                <Paper key={creneauHoraire.id} sx={{ p: 2, borderColor: "primary.main", borderWidth: "2px", borderStyle: "solid" }}>
                   <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }} display={"flex"} justifyContent={"center"}>
-                    {planning.title}
+                    {creneauHoraire.heureDebut}h - {creneauHoraire.heureFin}h
                   </Typography>
-                  <Typography variant="body1" component="div" display={"flex"} justifyContent={"center"}>
-                    {planning.content}
-                  </Typography>
+                  <Stack spacing={2}>
+                    <Typography variant="body1" component="div" display={"flex"} justifyContent={"center"} sx={{ fontWeight: postes.find((poste: Poste) => poste.id === myInscriptionBenevole.find((inscription: MonInscriptionBenevole) => inscription.creneauHoraireID === creneauHoraire.id)?.posteID) ? "bold" : "normal" }}>
+                      {postes.find((poste: Poste) => poste.id === myInscriptionBenevole.find((inscription: MonInscriptionBenevole) => inscription.creneauHoraireID === creneauHoraire.id)?.posteID)?.nom || "Aucun poste"}
+                    </Typography>
+                  </Stack>
                 </Paper>
               ))}
             </Stack>
@@ -131,13 +197,13 @@ export default function Home(){
           </Paper>
         </Stack>
         <Stack spacing={2} sx={{ width: "35%" }}>
-          <Paper sx={{ p: 2, backgroundColor: "secondary.main" }}>
+          <Paper sx={{ p: 2, borderColor: "secondary.main", borderWidth: "2px", borderStyle: "solid"}}>
             <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }} display={"flex"} justifyContent={"center"}>
               News
             </Typography>
             <Stack spacing={2}>
               {NEWS.map((news) => (
-                <Paper key={news.id} sx={{ p: 2, backgroundColor: "primary.main" }}>
+                <Paper key={news.id} sx={{ p: 2, borderColor: "primary.main", borderWidth: "2px", borderStyle: "solid" }}>
                   <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }} display={"flex"} justifyContent={"center"}>
                     {news.title}
                   </Typography>
@@ -148,13 +214,13 @@ export default function Home(){
               ))}
             </Stack>
           </Paper>
-          <Paper sx={{ p: 2, backgroundColor: "secondary.main" }}>
+          <Paper sx={{ p: 2, borderColor: "secondary.main", borderWidth: "2px", borderStyle: "solid" }}>
             <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }} display={"flex"} justifyContent={"center"}>
               Soirée
             </Typography>
             <Stack spacing={2}>
               {EVENTS.map((event) => (
-                <Paper key={event.id} sx={{ p: 2, backgroundColor: "primary.main" }}>
+                <Paper key={event.id} sx={{ p: 2, borderColor: "primary.main", borderWidth: "2px", borderStyle: "solid" }}>
                   <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }} display={"flex"} justifyContent={"center"}>
                     {event.title}
                   </Typography>
