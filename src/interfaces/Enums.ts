99075c594d60795c960f3e9b5575db0ef73696e0
Robinkss <robin.ave28@gmail.com>
2024-02-05 05:58:10 +0100

    Add enums

---
+++ src/interfaces/Enums.ts
@@ -0,0 +1,44 @@
+
+const tailleTShirt = [
+    {
+      value: 'XS',
+      label: 'XS',
+    },
+    {
+      value: 'S',
+      label: 'S',
+    },
+    {
+      value: 'M',
+      label: 'M',
+    },
+    {
+      value: 'L',
+      label: 'L',
+    },
+    {
+      value: 'XL',
+      label: 'XL',
+    },
+    {
+      value: 'XXL',
+      label: 'XXL',
+    },
+  ];
+  
+  const hebergement = [
+    {
+      value: 'Recherche',
+      label: 'Recherche',
+    },
+    {
+      value: 'Proposition',
+      label: 'Proposition',
+    },
+    {
+      value: 'Aucun',
+      label: 'Aucun',
+    },
+  ];
+
+  export { tailleTShirt, hebergement };
